3163e15cf2f86005683b4afafdbb3ebb5123db8a
Arthur <rthurmedeiros@gmail.com>
2023-11-15 15:52:58 -0300

    Tweaked story service

--- estimate/src/story/service.py
+++ estimate/src/story/service.py
@@ -21,15 +21,9 @@ class StoryService(BaseService):
 
     def get_query_column_converters(self) -> typing.Dict[str, typing.Callable[[any], str]]:
         return {
-            'name': from_str,
             'poker_id': from_uuid
         }
 
     def get_room_name(self, entity):
         story: Story = entity
         return str(story.poker_id)
-
-    @rpc
-    def vote(self, sid, channel: str, vote: str):
-        # TODO: move to EventService?
-        self.gateway_rpc.broadcast(channel, 'vote', vote)
