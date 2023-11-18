f33945ed384f707802525a5559547229f1892475
Arthur <rthurmedeiros@gmail.com>
2023-11-18 19:08:32 -0300

    Registered vote service to run

--- estimate/src/main.py
+++ estimate/src/main.py
@@ -2,3 +2,4 @@ from poker.service import PokerService
 from story.service import StoryService
 from participant.service import ParticipantService
 from event.service import EventService
+from vote.service import VoteService
