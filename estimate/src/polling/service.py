d79b946f8ebd7c43df749b65df4b861975f03efb
Arthur <rthurmedeiros@gmail.com>
2023-11-27 23:27:31 -0300

    Implemented also creating polling when story is created

--- estimate/src/polling/service.py
+++ estimate/src/polling/service.py
@@ -1,5 +1,8 @@
 import typing
 
+from nameko.rpc import rpc
+from nameko.events import event_handler
+
 from base.converters import from_uuid, from_bool
 from base.service import EntityService
 from polling.models import Polling
@@ -26,3 +29,20 @@ class PollingService(EntityService):
     def get_room_name(self, entity):
         polling: Polling = entity
         return f'story:{str(polling.story_id)}'
+
+    @event_handler("story_service", "story_created")
+    def handle_story_created(self, payload: dict):
+        # crates a new polling for every new story
+        self.create(sid=None, payload={
+            "storyId": payload['id']
+        })
+
+    @rpc
+    def restart(self, sid, story_id):
+        # TODO: complete old polling and creates a new one
+        pass
+
+    @rpc
+    def complete(self, sid, story_id):
+        # TODO: close the current polling. must define a value
+        pass
