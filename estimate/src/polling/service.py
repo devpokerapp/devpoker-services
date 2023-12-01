32cfc20e0f27f1efc1802c4fa871cc324f58a147
Arthur <rthurmedeiros@gmail.com>
2023-11-30 21:33:57 -0300

    Implemented method to retrieve current active polling

--- estimate/src/polling/service.py
+++ estimate/src/polling/service.py
@@ -1,12 +1,18 @@
 import typing
+import logging
+from typing import Optional
+from uuid import UUID
 
 from nameko.rpc import rpc
 from nameko.events import event_handler
 
+from base.exceptions import NotFound
 from base.converters import from_uuid, from_bool
 from base.service import EntityService
 from polling.models import Polling
-from polling.schemas import PollingRead, PollingCreate, PollingUpdate
+from polling.schemas import PollingRead, PollingCreate, PollingUpdate, PollingComplete
+
+logger = logging.getLogger(__name__)
 
 
 class PollingService(EntityService):
@@ -36,6 +42,23 @@ class PollingService(EntityService):
         self.create(sid=None, payload={
             "storyId": payload['id']
         })
+
+    @rpc
+    def current(self, sid, story_id):
+        story_id = UUID(story_id)
+
+        entity = self.db.query(Polling) \
+            .filter(Polling.story_id == story_id) \
+            .filter(Polling.completed == False) \
+            .order_by(Polling.created_at.desc()) \
+            .first()
+
+        if entity is None:
+            raise NotFound()
+
+        result = self.dto_read.to_json(entity)
+
+        return result
 
     @rpc
     def restart(self, sid, story_id):
--- estimate/test/test_polling_service.py
+++ estimate/test/test_polling_service.py
@@ -47,3 +47,50 @@ def test_when_retrieving_polling_should_return_as_dict(db_session):
     assert result['revealed'] is False
     service.gateway_rpc.unicast.assert_called_once()
     service.dispatch.assert_called_once()
+
+
+def test_when_getting_current_polling_should_return_last_not_completed_as_dict(db_session):
+    # arrange
+    fake_sid = '1aaa'
+    fake_poker_id = uuid.uuid4()
+    fake_story_id = uuid.uuid4()
+    fake_polling_id_completed = uuid.uuid4()
+    fake_polling_id_opened1 = uuid.uuid4()
+    fake_polling_id_opened2 = uuid.uuid4()
+    fake_polling_id_current = uuid.uuid4()
+
+    db_session.add(Poker(id=fake_poker_id, creator='user@test.com'))
+    db_session.commit()
+    db_session.add(Story(id=fake_story_id, name="Story 1", poker_id=fake_poker_id))
+    db_session.commit()
+    db_session.add(Polling(id=fake_polling_id_completed, story_id=fake_story_id, completed=True, revealed=True))
+    db_session.add(Polling(id=fake_polling_id_opened1, story_id=fake_story_id, completed=False, revealed=True))
+    db_session.add(Polling(id=fake_polling_id_opened2, story_id=fake_story_id, completed=False, revealed=True))
+    db_session.add(Polling(id=fake_polling_id_current, story_id=fake_story_id, completed=False, revealed=True))
+    db_session.commit()
+
+    service = worker_factory(PollingService, db=db_session)
+    service.gateway_rpc.unicast.side_effect = lambda *args, **kwargs: None
+    service.dispatch.side_effect = lambda *args, **kwargs: None
+
+    # act
+    result = service.current(fake_sid, str(fake_story_id))
+
+    # assert
+    assert type(result) is dict
+    assert 'id' in result
+    assert type(result['id']) is str
+    assert result['id'] == str(fake_polling_id_current)
+    assert 'storyId' in result
+    assert type(result['storyId']) is str
+    assert result['storyId'] == str(fake_story_id)
+    assert 'votes' in result
+    assert type(result['votes']) is list
+    assert 'value' in result
+    assert result['value'] is None
+    assert 'completed' in result
+    assert result['completed'] is False
+    assert 'revealed' in result
+    assert result['revealed'] is True
+    service.gateway_rpc.unicast.assert_not_called()
+    service.dispatch.assert_not_called()
